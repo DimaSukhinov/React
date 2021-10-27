5ad7e6962f96df13774b59bdd976731f480dc965
DimaSukhinov <dimasukhinov@yandex.by>
2021-10-28 01:32:47 +0300

    5.1 lessons

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import './App.css';
 import Rating from "./Components/Rating/Rating";
 import Accordion from "./Components/Accordion/Accordion";
 import AppTitle from "./Components/AppTitle";
+import { OnOff } from './Components/OnOff/OnOff';
 
 const App = () => {
     return (
         <div className="App">
             {/*<AppTitle/>
-            <Rating value={0}/>*/}
+            <Rating value={0}/>
             <Accordion titleValue={'Menu'} collapsed={false}/>
             <Accordion titleValue={'Menu 2'} collapsed={true}/>
             <Rating value={0}/>
@@ -16,7 +17,9 @@ const App = () => {
             <Rating value={2}/>
             <Rating value={3}/>
             <Rating value={4}/>
-            <Rating value={5}/>
+            <Rating value={5}/>*/}
+            <OnOff status={true}/>
+            <OnOff status={false}/>
         </div>
     );
 }
